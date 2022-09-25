466c9977deedee4e13e0fdd57ee9242f48e1e17b
Erick Loza <lozabucio.jony@gmail.com>
2022-09-24 21:18:55 -0500

    Add sign out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "inventory_files#index"
+  root to: redirect("/users/sign_in")
 
   devise_for :users
   resources :inventory_files, only: [:index, :create]
