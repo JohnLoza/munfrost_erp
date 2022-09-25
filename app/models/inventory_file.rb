259023c31318adc6027935bc7d7cc5e1720adb5a
Erick Loza <lozabucio.jony@gmail.com>
2022-09-24 23:35:42 -0500

    Send inventory to cyberpuerta

--- app/models/inventory_file.rb
+++ app/models/inventory_file.rb
@@ -12,12 +12,20 @@ class InventoryFile < ApplicationRecord
 
   def sync_to_cyberpuerta
     save_payload
+    Cyberpuerta::Sync.call(read_payload)
     update!(error_msg: nil, error_location: nil)
   rescue => error
     Rails.logger.error("Got an error while syncing to cyberpuerta")
     Rails.logger.error("#{error.message} | #{error.backtrace.first}")
     update!(error_msg: error.message, error_location: error.backtrace.first)
     raise(error)
+  end
+
+  def read_payload
+    return unless payload.attached?
+
+    raw_payload = File.read(payload_attachment_path)
+    JSON.parse(raw_payload)
   end
 
   private
@@ -31,6 +39,8 @@ class InventoryFile < ApplicationRecord
   end
 
   def save_payload
+    return if payload.attached?
+
     filename = "#{Time.now.to_i}.json"
     payload_filepath = Rails.root.join("tmp/#{filename}")
     cyberpuerta_payload = Cyberpuerta::Payload.call(file_attachment_path)
@@ -38,4 +48,3 @@ class InventoryFile < ApplicationRecord
     payload.attach(io: File.open(payload_filepath), filename: filename)
   end
 end
-
---
+++ app/services/cyberpuerta/sync.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "uri"
+require "json"
+require "net/http"
+
+module Cyberpuerta
+  class Sync < ApplicationService
+    def initialize(payload)
+      @payload = payload
+    end
+
+    def call
+      https = Net::HTTP.new(url.host, url.port)
+      https.use_ssl = true
+
+      request = Net::HTTP::Post.new(url)
+      request["Accept"] = "application/json"
+      request["Content-Type"] = "application/json"
+      request["Authorization"] = "Bearer #{ENV['CYBERPUERTA_TOKEN']}"
+      request.body = JSON.dump(@payload)
+
+      response = https.request(request)
+      parsed_response = JSON.parse(response.read_body)
+      return if parsed_response['message'] == 'Products uploaded successfully!'
+
+      raise(StandardError, parsed_response['message'])
+    end
+
+    private
+
+    def url
+      @url ||=
+        if Rails.env.production?
+          URI("https://cyberpuerta.mx/api/provider/articles/full-catalog")
+        else
+          URI("https://base.cyberpuerta.mx/api/provider/articles/full-catalog")
+        end
+    end
+  end
+end
